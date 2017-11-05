6c8788c3456fcbcd3be36c7e6c56e9b1b8b547f4
Jordan Dobney <jorddobbo@gmail.com>
2017-11-05 22:16:47 +0000

    Logo

--- app/public/wp-content/themes/aves-theme/single.php
+++ app/public/wp-content/themes/aves-theme/single.php
@@ -1,15 +1,5 @@
 <?php get_header() ?>
 
-<div class="title__banner">
-
-    <div class="container">
-        <div class="row">
-            <h1 class="page-title"><?php the_title(); ?></h1>
-        </div>
-    </div>
-</div>
-
-<div class="container">
 <?php while (have_posts()) : the_post(); ?>
     <article <?php post_class(); ?>>
         <header>
@@ -23,5 +13,4 @@
         </footer>
     </article>
 <?php endwhile; ?>
-</div>
 <?php get_footer() ?>
